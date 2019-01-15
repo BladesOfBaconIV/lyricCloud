8fc1e6110efe7bc5f94e238a008329ea9225f2a2
ewlolri <lorcan.williamson@ericsson.com>
2019-01-15 15:37:45 +0000

    Going through all albums (only getting 2 songs per album)

--- main.py
+++ main.py
@@ -2,36 +2,51 @@ from wordcloud import WordCloud
 from bs4 import BeautifulSoup
 from urllib import request
 from collections import Counter
+from os import path, makedirs
 
 URL_ROOT = 'http://www.sabaton.net'
-album_link = '/discography/heroes-album/'
 
-html = request.urlopen(URL_ROOT + album_link)
+if not path.exists('output'):
+    makedirs('output')
+
+html = request.urlopen(URL_ROOT + '/discography/')
 soup = BeautifulSoup(html, 'html.parser')
 
-link_htmls = soup.find_all('a', text='Lyrics')
+album_links_html = soup.find('div', class_='elementor-element elementor-element-353ce1e8 elementor-column elementor-col-50 elementor-top-column').find_all('a')
+album_links = [link['href'] for link in album_links_html]
 
-links = [link['href'] for link in link_htmls]
-
-song_lyrics = {}
-
-for link in links:
-    song_name = link.replace('/discography/', '').replace('/', '')
-    html = request.urlopen(URL_ROOT + link)
+def wordcloud_for_album(album_link):
+    html = request.urlopen(URL_ROOT + album_link)
     soup = BeautifulSoup(html, 'html.parser')
-    lyrics_box = soup.find_all('div', class_='elementor-text-editor elementor-clearfix')[1]
-    for br in lyrics_box.find_all('br'):
-        br.replace_with(' ')
-    text_blocks = lyrics_box.find_all('p')[1:] # skip the first one as it is publisher info
-    lyrics = ' '.join([verse.text for verse in text_blocks])
-    song_lyrics[song_name] = lyrics
 
-album_lyrics = ' '.join([track_lyrics for track_lyrics in song_lyrics.values()])
+    lyric_link_htmls = soup.find_all('a', text='Lyrics')
+    links = [link['href'] for link in lyric_link_htmls]
+    
+    album_name = album_link.replace('/discography/', '').replace('/', '')
+    print(album_name)
+    if not path.exists('output/' + album_name):
+        makedirs('output/' + album_name)
+    output_folder = 'output/' + album_name + '/'
 
-for song, lyrics in song_lyrics.items():
-    WordCloud().generate(lyrics).to_file('output/' + song + '.jpg')
+    song_lyrics = {}
+    for link in links:
+        song_name = link.replace('/discography/', '').replace('/', '')
+        print(song_name)
+        html = request.urlopen(URL_ROOT + link)
+        soup = BeautifulSoup(html, 'html.parser')
+        lyrics_box = soup.find_all('div', class_='elementor-text-editor elementor-clearfix')[1]
+        for br in lyrics_box.find_all('br'):
+            br.replace_with(' ')
+        text_blocks = lyrics_box.find_all('p')[1:] # skip the first one as it is publisher info
+        lyrics = ' '.join([verse.text for verse in text_blocks])
+        song_lyrics[song_name] = lyrics
 
-WordCloud().generate(album_lyrics).to_file('output/heroes.jpg')
+    album_lyrics = ' '.join([track_lyrics for track_lyrics in song_lyrics.values()])
 
-if 're' in album_lyrics:
-    print('why?')
+    for song, lyrics in song_lyrics.items():
+        WordCloud().generate(lyrics).to_file(output_folder + song_name + '.jpg')
+
+    WordCloud().generate(album_lyrics).to_file(output_folder + album_name + '.jpg')
+
+for album in album_links:
+    wordcloud_for_album(album)
